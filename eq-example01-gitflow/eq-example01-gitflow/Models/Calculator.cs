9c32812da1f5355aa51512c4c44ae8f5d5618210
Fábrica de Software CEET <vcortes@sena.edu.co>
2023-05-26 09:42:21 -0500

    feat(backend): Added ViewModel CalculatorViewModel

---
+++ eq-example01-gitflow/eq-example01-gitflow/Models/Calculator.cs
@@ -0,0 +1,17 @@
+﻿using eq_example01_gitflow.Interfaces;
+using System.ComponentModel.DataAnnotations;
+
+namespace eq_example01_gitflow.Models
+{
+    public class Calculator
+    {
+        [Required(ErrorMessage = "Field Number 1 is required")]
+        [RegularExpression(@"^\d+(\.\d+)?$", ErrorMessage = "Please enter a valid number for Number 1.")]
+        public decimal Number1 { get; set; }
+
+        [Required(ErrorMessage = "Field Number 2 is required")]
+        [RegularExpression(@"^\d+(\.\d+)?$", ErrorMessage = "Please enter a valid number for Number 2.")]
+
+        public decimal Number2 { get; set; }
+    }
+}
---
+++ eq-example01-gitflow/eq-example01-gitflow/Models/ViewModels/CalculatorViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace eq_example01_gitflow.Models.ViewModels
+{
+    public class CalculatorViewModel : Calculator
+    {
+        public string? Operacion { get; set; }
+    }
+}
